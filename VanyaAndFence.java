add2600f63b7db84568e40a82bdeddeb0d4e58bd
SOUMIK ROY <soumikroy7272@gmail.com>
2025-12-27 02:41:34 +0530

    Solved another question

---
+++ VanyaAndFence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class VanyaAndFence {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int total = 0;
+
+        for(int i=0; i<n; i++){
+            int friendH = sc.nextInt();
+            if(friendH>m){
+                total+=2;
+            }else{
+                total+=1;
+            }
+        }
+        System.out.println(total);
+    }
+}
