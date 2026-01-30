95b6d82b932e78a75206e63832d9fb623702b67b
SOUMIK ROY <soumikroy7272@gmail.com>
2026-01-30 21:07:23 +0530

    Solved another one

---
+++ PorM.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class PorM {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        if(sc.hasNextInt()){
+            int t = sc.nextInt();
+            while(t-->0){
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+                if(a+b == c){
+                    System.out.println("+");
+                }else{
+                    System.out.println("-");
+                }
+            }
+        }
+    }
+}
