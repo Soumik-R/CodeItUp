211a1b819683b03f58cc49e837c151a60e47c433
SOUMIK ROY <soumikroy7272@gmail.com>
2026-02-11 01:09:46 +0530

    Another One

--- ArrayColor.java
+++ ArrayColor.java
@@ -2,23 +2,15 @@ import java.util.Scanner;
 public class ArrayColor {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        if(!sc.hasNextInt()) return;
         int t = sc.nextInt();
         while(t-->0){
             int n = sc.nextInt();
-            int[] a = new int[n];
-            boolean ptrn1 = true;
-            boolean ptrn2 = true;
-            for(int i =0; i<n; i++){
-                a[i] = sc.nextInt();
-                if(a[i] % 2 != i%2){
-                    ptrn1 = false;
-                }
-                if(a[i] %2 == i%2){
-                    ptrn2 = false;
-                }
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                sum = sum + sc.nextInt();
             }
-            if(ptrn1||ptrn2){
+
+            if(sum%2 == 0){
                 System.out.println("YES");
             }else{
                 System.out.println("NO");
