57e8b70f8c881493d6ad99b671a3fafcfabd984d
SOUMIK ROY <soumikroy7272@gmail.com>
2026-02-13 23:39:09 +0530

    Another one

---
+++ BlankSpace.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class BlankSpace {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int maxZeros = 0;
+            int currentZeros = 0;
+            for (int i = 0; i < n; i++) {
+                int val = sc.nextInt();
+                if (val == 0) {
+                    currentZeros++;
+                    if (currentZeros > maxZeros) {
+                        maxZeros = currentZeros;
+                    }
+                } else {
+                    currentZeros = 0;
+                }
+            }
+            System.out.println(maxZeros);
+        }
+    }
+}
