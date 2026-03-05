a36c1e53c09d8b46013429cc9a4171e98234bc88
SOUMIK ROY <soumikroy7272@gmail.com>
2026-03-05 19:54:23 +0530

    Updated

--- BlankSpace.java
+++ BlankSpace.java
@@ -13,7 +13,7 @@ public class BlankSpace {
                 if (val == 0) {
                     currentZeros++;
                     if (currentZeros > maxZeros) {
-                        maxZeros = currentZeros;H
+                        maxZeros = currentZeros;
                     }
                 } else {
                     currentZeros = 0;
